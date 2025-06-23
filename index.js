b564c3b6a13eda74eec41c0b9c6a7617d8e2877d
Andy-Ops254 <ghostandy254@outlook.com>
2025-06-23 21:33:07 +0300

    create post card

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let postList = document.getElementById('post-list');
+let postContainer = document.getElementById('post-detail');
+let postForm = document.getElementById('newPost');
+
+function addPostListener() {
+    postForm.addEventListener('submit',e => {
+        e.preventDefault();
+        
+    }); 
+};
 
 function handlePostClick(postID) {
     fetch(`http://localhost:3000/Posts/${postID}`)
     .then(resp => resp.json())
     .then(post => {
-        console.log(post);
+        postContainer.innerHTML = `
+        <p>${post.title}</p>
+        <p>${post.author}</p>
+        <p>${post.content}</p>
+        `
     });
 };
 
